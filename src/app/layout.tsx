7f46a59e63c626142ebbb96db5c2f6d2f7a41e19
Mayur Bhatt <mayur27021991@gmail.com>
2025-04-15 14:36:33 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `WP Tangerine Blogs`,
+  description: `Latest Blogs and articles from WP Tangerine.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
